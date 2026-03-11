f2d4682b73a56740b818820568c4f436ad6c02c3
Trần Nguyên <nguyen3503@tdkn3.local>
2026-03-11 13:58:55 +0300

    new init

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,3 @@
-# from flask import Flask, request, render_template
-
-# app = Flask(__name__, template_folder ='templates')
-
-# #Load the configuration from the config.py file
-
-# @app.route("/")
-# def home():
-#     return render_template('home.html')
-# @app.route('/tournaments')
-# def tournaments():
-#     return render_template('tournaments.html')
-
-# @app.route('/teams')
-# def teams():
-#     return render_template('teams.html')
-
-# @app.route('/matches')
-# def matches():
-#     return render_template('matches.html')
-
-# @app.route('/standings')
-# def standings():
-#     return render_template('standings.html')
-   
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port = 5555, debug=True)
-
 from flask import Flask
 from app.models import db
 from config import config
