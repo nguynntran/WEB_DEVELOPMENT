4f1359bc50eb76d698a6619a68f246043f374161
Trần Nguyên <nguyen3503@tdkn3.local>
2026-04-14 10:15:22 +0300

    new

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,30 +2,27 @@ from flask import render_template
 
 
 def register_routes(app, db, bcrypt):
-    """Register all routes to the Flask app"""
+    #Register all routes to the Flask app
     
     # Home route
     @app.route("/", endpoint='home')
     def home():
         return render_template('home.html')
     
-    # Import and register tournament routes
+    # Import and register tournament, team, match, standing and authorization routes
+    
     from .tournament_routes import register_tournament_routes
     register_tournament_routes(app, db)
     
-    # Import and register team routes
     from .team_routes import register_team_routes
     register_team_routes(app, db)
     
-    # Import and register match routes
     from .match_routes import register_match_routes
     register_match_routes(app, db)
     
-    # Import and register standing routes
     from .standing_routes import register_standing_routes
     register_standing_routes(app, db)
     
-    # Import and register auth routes
     from .auth_routes import register_auth_routes
     register_auth_routes(app, bcrypt)
 
