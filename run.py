4575913e37f4a272ad617cc0bde036503ccbd3aa
Trần Nguyên <nguyen3503@tdkn3.local>
2026-05-12 10:09:28 +0300

    new run.py file

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from app import create_app
 import os
 
+from app import create_app
 
-app = create_app(os.getenv('FLASK_ENV', 'development'))
+app = create_app(os.getenv("FLASK_ENV", "development"))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, debug=True)
